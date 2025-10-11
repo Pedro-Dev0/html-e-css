15317f2333137e36bea3a1da9efc04f2d5d5dbe3
Pedro Sampaio <208987957+Pedro-Dev0@users.noreply.github.com>
2025-10-11 18:40:04 -0300

    js

---
+++ html/DIO/HTML/Logica/condicionais.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+
+let ehligado = false
+
+if(1 === "1") {  //if só true
+    console.log("executar comando")
+} else {
+    console.log("continua desligado")
+}  //else só false( quando a condição não é compromida)
+
+
+let possuiOvos = false
+let itensComprados = ""
+
+if(possuiOvos) {
+    itensComprados = "Leite"
+    console.log("item comprado: " + itensComprados)
+} else {
+    console.log("Comprar ovos e leite")
+}

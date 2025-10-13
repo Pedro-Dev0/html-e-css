19f8f29a376232e458e623f97c355873c724d6a5
Pedro Sampaio <208987957+Pedro-Dev0@users.noreply.github.com>
2025-10-12 22:06:13 -0300

    js

---
+++ html/DIO/HTML/Logica/teste.js
@@ -0,0 +1,35 @@
+
+const vovoTem = "Na casa da minha vó tem, "
+let cafe = "Cafe, "
+let acucar = "Açucar, "
+let bicicleta = "Minha bicicleta "
+
+console.log(vovoTem + cafe + acucar + bicicleta)
+console.log("E por isso gosto de ir lá para brincar muito")
+
+let brincar = ["Pular corda", "Pega pega", "corrida"]
+
+console.log(brincar[2])
+
+let colegas = [
+    ["João", "Idade: 10", "Legal"],
+    ["Carol", "Idade: 9", "Bacaninha"],
+    ["Carlos", "Idade: 13", "Chato"],
+]
+
+console.log("Gostaria de brincar com, " + colegas[0][0] + " porque acho ele muito " + colegas[0][2] + " Juntos vamos brincar de " + brincar[2])
+
+colegas.push(["Maria", "Idade: 9", "Linda"])
+
+console.log(colegas)
+colegas.splice(3)
+
+console.log(colegas)
+
+let vamosBrincar = true
+
+if(vamosBrincar == true) {
+    console.log("Arrumou a casa pode ir brincar")
+} else {
+    console.log("Não fez nada, não pode brincar")
+}

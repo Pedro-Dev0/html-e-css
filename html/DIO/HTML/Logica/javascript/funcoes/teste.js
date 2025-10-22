ec56eaa7bbf2aa7a9e89af24c43f62ac0805f03a
Pedro Sampaio <208987957+Pedro-Dev0@users.noreply.github.com>
2025-10-21 21:24:55 -0300

    Update teste.js

--- html/DIO/HTML/Logica/javascript/funcoes/teste.js
+++ html/DIO/HTML/Logica/javascript/funcoes/teste.js
@@ -1,28 +1,30 @@
 function myNameIs(name) {
-  console.log(`Meu nome é ` + name);
+  return `Meu nome é ` + name;
 }
 
-myNameIs(`Pedro`);
-
 function verificarIdade(idade) {
-    if(idade < 18) {
-        console.log(`Menor de idade, pois sua idade é ` + idade );
-    } else if(idade >= 18 && idade < 40) {
-        console.log(`Adulto pois sua idade é ` + idade );
-    } else {
-        console.log(`Senhor pois suda idade é ` + idade);
-    }
+  if (idade < 18) {
+    console.log(
+      myNameIs(`Pedro, `) + `Menor de idade pois sua idade é ` + idade
+    );
+  } else if (idade >= 18 && idade < 40) {
+    console.log(myNameIs(`Pedro, `) + `Adulto pois sua idade é ` + idade);
+  } else {
+    console.log(myNameIs(`Pedro, `) + `Senhor pois suda idade é ` + idade);
+  }
 }
 
 function podeBeber(idade) {
-    verificarIdade(idade)
-    if (idade < 18) {
-        console.log(`Criança demais para isso vá para casa`);
-    } else {
-        console.log(`Finalmente um homem, afunda esse barril gragolândia`);
-    }
+  verificarIdade(idade);
+  if (idade < 18) {
+    console.log(`Criança demais para isso vá para casa`);
+  } else {
+    console.log(`Finalmente um homem, afunda esse barril gragolândia`);
+  }
 }
 
-podeBeber(18);
+function tudoJunto() {
+  podeBeber(17);
+}
 
-
+tudoJunto();

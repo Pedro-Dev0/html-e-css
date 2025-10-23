13ed73ad0337e0247fa14aebb9bf27ae77fd7855
Pedro Sampaio <208987957+Pedro-Dev0@users.noreply.github.com>
2025-10-23 14:31:11 -0300

    Create objeto.js

---
+++ html/DIO/HTML/Logica/javascript/objetos/objeto.js
@@ -0,0 +1,48 @@
+// objeto literal abaixo:
+const pedro = {
+  nome: "Pedro Sampaio",
+  idade: 22,
+  descricao: function () {
+    //this assume o objeto e função para melhor funcionamento...
+    console.log(
+      `Meu nome é ${this.nome}, e tenho ${this.idade} anos, atualmente procuro vagas de estágio em TI para melhorar na área e me consolidar na mesma, conseguindo então maiores experiências e me aprimorando profissionalmente`
+    );
+  },
+};
+
+console.log(pedro.nome);
+console.log(pedro.idade);
+console.log(pedro);
+
+pedro.altura = 1.9;
+console.log(pedro);
+
+//delete pedro.idade; para deletar informação do objeto
+
+pedro.descricao();
+
+pedro.nome = "Davi";
+pedro.idade = 30;
+
+pedro.descricao();
+
+pedro.descricao = function () {
+  console.log(`Fala para bexiga meu DEUS... seu nome é ${this.nome}`);
+};
+
+pedro.descricao();
+
+const atributo = "idade"; // usando uma string com o nome igual ao do atributo no objeto para poder puxar ele de uma forma dinamica, sem precisar ser diretamente chamado...
+
+console.log(pedro[atributo]);
+
+class pessoa {
+  nome;
+  idade;
+
+  descricao() {
+    console.log(
+      `Meu nome é ${this.nome}, e tenho ${this.idade} anos, atualmente procuro vagas de estágio em TI para melhorar na área e me consolidar na mesma, conseguindo então maiores experiências e me aprimorando profissionalmente`
+    );
+  }
+};

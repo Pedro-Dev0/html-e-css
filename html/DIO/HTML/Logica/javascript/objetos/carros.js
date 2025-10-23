23c6e05c8c83ce524b23a91f66e7f3be1f81f7ff
Pedro Sampaio <208987957+Pedro-Dev0@users.noreply.github.com>
2025-10-23 17:16:28 -0300

    Update carros.js

---
+++ html/DIO/HTML/Logica/javascript/objetos/carros.js
@@ -0,0 +1,22 @@
+class Carro {
+  marca;
+  cor;
+  gastoMedioPorKm;
+  // classe para definição
+  constructor(marca, cor, gastoMedioPorKm) {
+    this.marca = marca;
+    this.cor = cor;
+    this.gastoMedioPorKm = gastoMedioPorKm.toFixed(2);
+  }
+
+  calcularGastoDePercurso(distanciaEmKm, precoDoCombustivel) {
+    return distanciaEmKm * this.gastoMedioPorKm * precoDoCombustivel;
+  }
+}
+
+const uno = new Carro(`Fiat`, `Branco`, 10 / 14); // instancia
+console.log(uno);
+console.log(uno.calcularGastoDePercurso(10, 6.22));
+const palio = new Carro(`Volksvagew`, `Prata`, 10 / 12.5)
+console.log(palio);
+console.log(palio.calcularGastoDePercurso(10, 6.22));

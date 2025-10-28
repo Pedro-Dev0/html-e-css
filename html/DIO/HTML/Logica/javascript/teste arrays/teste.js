6c06885dcae22dde6dd9c1d5b81e89dfd622bf13
Pedro Sampaio <208987957+Pedro-Dev0@users.noreply.github.com>
2025-10-27 21:36:10 -0300

    Update teste.js

--- html/DIO/HTML/Logica/javascript/teste arrays/teste.js
+++ html/DIO/HTML/Logica/javascript/teste arrays/teste.js
@@ -1,5 +1,13 @@
-for (i = 0; i <= 10; i++) {
+/*for (i = 0; i <= 10; i++) {
   console.log(i * 8);
 }
+*/
 
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let z = 0; z <= numeros.length; z++) {
+    const numero = numeros[z];
+    if(numero % 2 === 0) {
+        console.log(numero);
+    }
+}

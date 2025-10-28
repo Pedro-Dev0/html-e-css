c5b97e020285668dea1487e9f35e0868e65e66b8
Pedro Sampaio <208987957+Pedro-Dev0@users.noreply.github.com>
2025-10-27 21:50:21 -0300

    Update teste.js

--- html/DIO/HTML/Logica/javascript/teste arrays/teste.js
+++ html/DIO/HTML/Logica/javascript/teste arrays/teste.js
@@ -5,7 +5,7 @@
 
 const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (let z = 0; z <= numeros.length; z++) {
+for (let z = 0; z < numeros.length; z++) {
     const numero = numeros[z];
     if(numero % 2 === 0) {
         console.log(numero);
